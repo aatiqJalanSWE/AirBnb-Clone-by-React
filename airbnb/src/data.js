914fa5af37508e536db31ec2e4ac5675e74db0c6
aatiqJalanSWE <aatiq.afzal@jalantechnologies.com>
2023-04-12 20:00:56 +0530

    comments

--- airbnb/src/data.js
+++ airbnb/src/data.js
@@ -1,44 +1,80 @@
-import cardImage1 from "./images/katie-zaferes.jpeg"
-import cardImage2 from "./images/wedding-photography.jpg"
-import cardImage3 from "./images/mountain-bike.jpeg"
+import cardImage1 from "./images/katie-zaferes.jpeg";
+import cardImage2 from "./images/wedding-photography.jpg";
+import cardImage3 from "./images/mountain-bike.jpeg";
 
 const data = [
-    {
-        id: 1,
-        title: "Life Lessons with Katie Zaferes",
-        price: 136,
-        coverImg: cardImage1,
-        stats: {
-            rating: 5.0,
-            reviewCount: 6
-        },
-        location: "Online",
-        openSpots: 0,
+  {
+    id: 1,
+    title: "Life Lessons with Katie Zaferes",
+    price: 136,
+    coverImg: cardImage1,
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
     },
-    {
-        id: 2,
-        title: "Learn Wedding Photography",
-        price: 125,
-        coverImg: cardImage2,
-        stats: {
-            rating: 5.0,
-            reviewCount: 30
-        },
-        location: "Online",
-        openSpots: 27,
+    location: "Online",
+    openSpots: 0,
+  },
+  {
+    id: 2,
+    title: "Learn Wedding Photography",
+    price: 125,
+    coverImg: cardImage2,
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
     },
-    {
-        id: 3,
-        title: "Group Mountain Biking",
-        price: 50,
-        coverImg: cardImage3,
-        stats: {
-            rating: 4.8,
-            reviewCount: 2
-        },
-        location: "Norway",
-        openSpots: 3,
-    }
-]
+    location: "Online",
+    openSpots: 27,
+  },
+  {
+    id: 3,
+    title: "Group Mountain Biking",
+    price: 50,
+    coverImg: cardImage3,
+    stats: {
+      rating: 4.8,
+      reviewCount: 2,
+    },
+    location: "Norway",
+    openSpots: 3,
+  },
+  //     {
+  //         id: 4,
+  //         title: "Group Mountain Biking",
+  //         price: 50,
+  //         coverImg: cardImage3,
+  //         stats: {
+  //             rating: 4.8,
+  //             reviewCount: 2
+  //         },
+  //         location: "Norway",
+  //         openSpots: 3,
+  //     },
+  //     {
+  //         id: 3,
+  //         title: "Group Mountain Biking",
+  //         price: 50,
+  //         coverImg: cardImage3,
+  //         stats: {
+  //             rating: 4.8,
+  //             reviewCount: 2
+  //         },
+  //         location: "Norway",
+  //         openSpots: 3,
+  //     },
+  //     {
+  //         id: 3,
+  //         title: "Group Mountain Biking",
+  //         price: 50,
+  //         coverImg: cardImage3,
+  //         stats: {
+  //             rating: 4.8,
+  //             reviewCount: 2
+  //         },
+  //         location: "Norway",
+  //         openSpots: 3,
+  //     },
+];
 
-export default data
+export default data;
